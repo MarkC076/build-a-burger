53151287951c9110345da449bbc947c704037aad
markchilton <mark.chilton@itcuk.com>
2019-01-08 15:07:11 +0000

    Added axios and spinner

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger"
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -84,7 +85,24 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        alert("You Continue");
+        // alert("You Continue");
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: { 
+                name: "Mark Chilton",
+                address: {
+                    street: "Test Street 1",
+                    zipCode: "SK67GA",
+                    country: "UK"
+                },
+                email: "test@test.com"
+             },
+             deliveryMethod: "fastest"
+        }
+        axios.post("/orders.json", order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     };
 
     render () {
